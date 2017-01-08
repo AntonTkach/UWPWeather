844273d281640e7ed19ed56f615b194dae62967a
Anton Tkach <dekerter@gmail.com>
2017-01-08 19:33:01 +0200

    Working app. Without a tile

--- UWPWeather/UWPWeather_Bob2/MainPage.xaml.cs
+++ UWPWeather/UWPWeather_Bob2/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,20 +33,34 @@ namespace UWPWeather_Bob2
         {
             var position = await LocationManager.GetPosition();
 
+            var lat = position.Coordinate.Latitude;
+            var lon = position.Coordinate.Longitude;
+
             RootObject myWeather =
                 await OpenWeatherMapProxy.GetWeather(
-                    position.Coordinate.Latitude,
-                    position.Coordinate.Longitude);
+                    lat,
+                    lon);
+
+            //Shedule update
+            // website is down
+            var uri = String.Format("http://uwpweatherservice.azurewebsites.net/?lat={0}&lon={1}", lat, lon);
+
+            var tileContent=new Uri(uri);
+            var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
+
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.StartPeriodicUpdate(tileContent, requestedInterval);
 
 
             string icon = String.Format(
                 "ms-appx:///Assets/Weather/{0}.png", 
                 myWeather.weather[0].icon);
             ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
-            ResultTextBlock.Text = myWeather.name 
-                + " - " 
+            ResultTextBlock.Text = 
+                myWeather.name 
+                + " - Current temprature: " 
                 + ((int)myWeather.main.temp).ToString() 
-                + " - " + myWeather.weather[0].description;
+                + " C° - " + myWeather.weather[0].description;
 
         }
     }
